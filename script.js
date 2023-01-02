677e20a91703c1d0fd34bb92b694b9587614b42a
Aechelon-Online <info@aechelon.online>
2023-01-02 12:20:08 -0600

    Fixed button width on mobile size

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const checkboxes = document.querySelector(".entertainment-checkboxes")
 const radios = document.querySelector(".event-radio-buttons")
+const list = document.querySelector(".list")
+const dots = document.querySelector(".dots")
+const eventSelect = document.querySelector(".event-select")
+const serviceSelect = document.querySelector(".service-select")
+
+let isOpen = true
 
 checkboxes.addEventListener('click', () => {
-    document.querySelector(".list").classList.toggle("show")
+    list.classList.toggle("show")
+    isOpen = !isOpen
+    serviceSelect.innerText = isOpen ? "▼" : "✕"
 })
 
 radios.addEventListener('click', () => {
-    document.querySelector(".dots").classList.toggle("show")
+    dots.classList.toggle("show")
+    isOpen = !isOpen
+    eventSelect.innerText = isOpen ? "▼" : "✕"
 })
 
 
